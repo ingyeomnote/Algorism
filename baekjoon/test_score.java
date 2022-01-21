edc718f77b6a31ebab38b15e790a6e213754ec80
ingyeomnote <79016263+ingyeomnote@users.noreply.github.com>
2022-01-22 00:48:06 +0900

    Scanner -> BufferedReader로 변경
    Buffer을 사용해야 처리 속도가 빨라져서 변경했습니다.

--- baekjoon/test_score.java
+++ baekjoon/test_score.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+import java.io.*;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int point = sc.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int point = Integer.parseInt(br.readLine());
         String grade;
         
         switch(point/10){
