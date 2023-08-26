be6416c4a524dc91153c48e2cd5111877bef9266
KimInKyeom <freeartjin@naver.com>
2023-08-26 14:13:29 +0900

    경로 변경으로 인해 삭제

--- programmers/lv_1/running_races/Solution.java
+++ programmers/lv_1/running_races/Solution.java
@@ -1,4 +1,4 @@
-package Algorism.programmers.lv_1;
+package Algorism.programmers.lv_1.running_races;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -6,9 +6,9 @@ class Solution {
     /**
     * 달리기 경주
     *
-    * @param String[] players 매개변수는 선수들의 이름이 1등부터 현재 등수 순서대로 담긴 문자열 배열이다.
-    * @param String[] callilngs 매개변수는 해설진이 부른 이름을 담은 문자열 배열이다.
-    * @return 경주가 끝났을 때 선수들의 이름을 1등부터 등수 순서대로 정렬한 배열을 반환한다.
+    * @param String[] players 매개변수는 선수들의 이름이 1등부터 현재 등수 순서대로 담긴 문자열 배열이다
+    * @param String[] callilngs 매개변수는 해설진이 부른 이름을 담은 문자열 배열이다
+    * @return 경주가 끝났을 때 선수들의 이름을 1등부터 등수 순서대로 정렬한 배열을 반환한다
     */
     public String[] solution(String[] players, String[] callings) {
         String[] answer = new String[players.length];
